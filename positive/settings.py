c21731638e9e46a56cf8b0137c9dfed8334efe24
Hung Tran <hung@traan.vn>
2016-11-20 03:18:59 -0500

    Partials and frontend organized

--- positive/settings.py
+++ positive/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-	'api.apps.ApiConfig',
+    'api.apps.ApiConfig',
+	'frontend.apps.ApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
