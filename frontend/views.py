57ab68c57d5feff47c6b04e1d820fae130f3661a
Hung Tran <hung@traan.vn>
2016-11-20 07:18:46 -0500

    Added exit button, return addr

--- frontend/views.py
+++ frontend/views.py
@@ -1,13 +1,55 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home-positiv.html', {})
+	d = {}
+	d['title'] = 'Home'
+	d['exit_button'] = False
+	d['name'] = 'Hung'
+
+	return render(request, 'home-positiv.html', d)
 
 def diary(request):
-    return render(request, 'entries.html', {})
-    
+	d = {}
+	d['title'] = 'Diary'
+	d['exit_button'] = False
+	return render(request, 'entries.html', d)
+	
 def graphs(request):
-    return render(request, 'graphs.html', {})
-    
+	d = {}
+	d['title'] = 'Graphs'
+	d['exit_button'] = False
+	return render(request, 'graphs.html', d)
+	
 def profile(request):
-    return render(request, 'profile.html', {})
+	d = {}
+	d['title'] = 'Profile'
+	d['exit_button'] = False
+	return render(request, 'profile.html', d)
+
+def reflection1(request):
+	d = {}
+	d['title'] = 'Reflection 1'
+	d['returning_addr'] = '/'
+	d['exit_button'] = True
+	return render(request, 'reflection-1.html', d)
+
+def reflection2(request):
+	d = {}
+	d['title'] = 'Reflection 1'
+	d['returning_addr'] = '/'
+	d['exit_button'] = True
+	return render(request, 'reflection-2.html', d)
+
+def reflection3(request):
+	d = {}
+	d['title'] = 'Reflection 1'
+	d['returning_addr'] = '/'
+	d['exit_button'] = True
+	return render(request, 'reflection-3.html', d)
+
+def reflection4(request):
+	d = {}
+	d['title'] = 'Reflection 1'
+	d['returning_addr'] = '/'
+	d['exit_button'] = True
+	return render(request, 'reflection-4.html', d)
